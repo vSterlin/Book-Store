1a066f7ad2efaebaa714fa8c7995a02e19e9534d
Vladimir Sterlin <vsterlin21@gmail.com>
2021-10-17 00:52:59 -0700

    struct tags for json

--- entity/book.go
+++ entity/book.go
@@ -1,9 +1,9 @@
 package entity
 
 type Book struct {
-	Id          int
-	Author      string
-	Title       string
-	Pages       int
-	PublishDate string
+	Id          int    `json:"id"`
+	Author      string `json:"author"`
+	Title       string `json:"title"`
+	Pages       int    `json:"pages"`
+	PublishDate string `json:"publishDate"`
 }
